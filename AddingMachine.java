4e9ba20a2ad8116a2b5dc8ec4e4b82f0de66bf2e
Siva <snnagula@asu.edu>
2019-10-09 23:13:08 -0700

    commit after doing part 1

---
+++ AddingMachine.java
@@ -0,0 +1,67 @@
+/*
+ * Name: Sivanarayana Nagulapati
+ * class ID: 1213080048
+ * Assignment number: 2
+
+*/
+
+
+
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total; //declaring a variable
+	
+	
+	/* creating a constructor and 
+	 * initializing the variable to zero  
+	 */
+	public AddingMachine () {
+	
+		total = 0;  // not needed - included for clarity
+		
+	}
+	/*
+	 * this method is to return total
+	 */
+	public int getTotal () {
+	
+		return 0;
+		}
+	/*
+	 * creating a method add 
+	 * 
+	 * @param taking an integer as its parameter
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * creating a method to subtract
+	 * 
+	 * @param taking an integer as its parameter
+	 */
+	
+	public void subtract (int value) {
+		
+		
+	}
+	/*
+	 * creating a method toString
+	 * 
+	 * to return all the methods
+	 */
+	public String toString () {
+		
+		return "";
+		
+	}
+	/*
+	 * creating a method clear
+	 */
+	public void clear() {
+	
+	}
+}
