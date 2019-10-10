0dcac74676b7e2ed720b67bf2d99a38173676446
Siva <snnagula@asu.edu>
2019-10-09 23:27:28 -0700

    after doing part 2

--- AddingMachine.java
+++ AddingMachine.java
@@ -8,60 +8,74 @@
 
 
 package cse360assign2;
+/* creating a class AddingMachine
+ * 
+ * and declaring two private variables
+ */
 
 public class AddingMachine {
 	
-	private int total; //declaring a variable
+    private int total;// deaclaring a private variable total
 	
+	private String history;// decalring a private String history
 	
-	/* creating a constructor and 
-	 * initializing the variable to zero  
-	 */
-	public AddingMachine () {
-	
-		total = 0;  // not needed - included for clarity
-		
-	}
 	/*
-	 * this method is to return total
-	 */
-	public int getTotal () {
-	
-		return 0;
-		}
-	/*
-	 * creating a method add 
+	 * Creating a constructor and initializing 
+	 * values to the variables declared
 	 * 
-	 * @param taking an integer as its parameter
-	 */
-	public void add (int value) {
-		
-	}
-	
-	/*
-	 * creating a method to subtract
-	 * 
-	 * @param taking an integer as its parameter
 	 */
 	
-	public void subtract (int value) {
-		
+	public AddingMachine() {
+		total=0;// declaring total to zero
+		history="0";// declaring history to null
 		
 	}
 	/*
-	 * creating a method toString
-	 * 
-	 * to return all the methods
+	 * creating a method add
+	 * @param value adding this value to the total and history
 	 */
-	public String toString () {
-		
-		return "";
-		
-	}
-	/*
-	 * creating a method clear
-	 */
-	public void clear() {
 	
-	}
+    public void add(int value) {
+        total += value;// adding total to total and value
+        history += " + " + value; // Concatenating  history to value
+    }
+    /*
+	 * creating a method subtract to subtract the value
+	 * @param value adding this value to the total and history
+	 */
+    public void subtract(int value) {
+        total -= value;// subtracting value from total
+        history += " - " + value;// concatenating history to the value
+    }
+    /*
+     * creating a method getTotal
+     * to return the total value
+     * 
+     */
+    public int getTotal(){
+  
+    	return total;
+    	
+    	
+    }
+    /*
+     * crating method toSrtring to return the string value
+     */
+    public String toString () {
+    
+    	return history;
+    	
+    }
+    /*
+     * creating a method clear
+     * and initializing total and history to zero
+     */
+    public void clear() {
+    	total=0;
+		history="0";
+		
+    	
+ }
+     
+
 }
